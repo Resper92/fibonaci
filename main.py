3db5b500c6fd751d5f2614d8fac4bc83ced8d1f4
Roman Romanovsky <romanromnovskyy@gmail.com>
2025-01-31 18:12:31 +0100

    commit

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 import memcache
 
 
-cache = memcache.Client(['127.0.0.1:11211'], debug=0)
+cache_client = memcache.Client([('127.0.0.1', 11211)])
 
-for cache_key in cache.keys():
-    print(cache_key)
+def memorize(func):
+    def wrapper(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        result = cache_client.get(key)
+        if result is None:
+            result = func(*args, **kwargs)
+            cache_client.set(key, result,30)
+        return result
+    return wrapper
 
-# def cache (func):
-#     cache = memcache.Client(['127.0.0.1:11211'], debug=0)
-#     def wrapper(*args, **kwargs):
-#         key = str(args) + str(kwargs)
-#         result = cache.get(key)
-#         if result is None:
-#             result = func(*args, **kwargs)
-#             cache.set(key, result)
-#         return result
+@memorize
+def number_fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    return number_fibonacci(n - 1) + number_fibonacci(n - 2)
 
-#     return wrapper
 
-# @cache
-# def number_fibonaci(n):
-#     if n == 1:
-#         return 1
-#     elif n == 2:
-#         return 1
-#     else:
-#         return number_fibonaci(n - 1) + number_fibonaci(n - 2)
-    
-# for i in range(1,100000):
-#     print(number_fibonaci(i))
+for i in range(1, 10000):  
+    print(f"Fibonacci({i}) = {number_fibonacci(i)}")
